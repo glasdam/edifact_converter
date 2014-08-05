8af068d66b9acfcafbc29e64b929f1edbc0e3167
Jacob Glasdam <jag@medcom.dk>
2014-08-05 09:49:45 +0200

    Start

---
+++ lib/edifact_converter/position.rb
@@ -0,0 +1,21 @@
+
+module EdifactConverter
+
+  class Position
+    attr_reader :line, :column
+
+    def initialize(line = 0, column = 0)
+      @line, @column = line, column
+    end
+
+    def ==(other)
+      @line == other.line and @column == other.column
+    end
+
+    def to_s
+      "At line #{@line}, column #{@column}"
+    end
+
+  end
+
+end
